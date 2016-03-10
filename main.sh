14369c270cdaaa97a08a589bd82fd93f25b7c242
Yuri Claure <yuriclaure@gmail.com>
2016-03-10 11:05:27 -0400

    Arrumando nome dos arquivos

--- main.sh
+++ main.sh
@@ -2,18 +2,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/checks.sh
 source $DIR/common.sh
+source $DIR/configure.sh
 source $DIR/core.sh
 source $DIR/create.sh
 source $DIR/finish.sh
 source $DIR/help.sh
-source $DIR/install.sh
 source $DIR/push.sh
 source $DIR/route.sh
 
-if ! [ -f $DIR/config.sh ]; then
+if ! [ -f $DIR/settings.sh ]; then
 	__configure;
 fi
-source $DIR/config.sh
+source $DIR/settings.sh
 
 alias codereview=__codereview
 alias cr=__codereview
