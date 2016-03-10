6c178a79a61f529f442f9c0f6180722d2ac21278
Yuri Claure <yuriclaure@gmail.com>
2016-03-10 11:07:49 -0400

    Arrumando bugs

--- configure.sh
+++ configure.sh
@@ -5,8 +5,8 @@ __configure () {
 	__get_required_input "URL do TFS (Exemplo: http://<url-do-tfs>/<collection>/<project>)";
 	codereview_base_url=$REPLY
 
-	echo "codereview_base_url_without_project=\"${codereview_base_url%/*}\"" > $DIR/config.sh
-	echo "declare -A repositories_id;" >> $DIR/config.sh
+	echo "codereview_base_url_without_project=\"${codereview_base_url%/*}\"" > $DIR/settings.sh
+	echo "declare -A repositories_id;" >> $DIR/settings.sh
 	
 	json=$(curl --ntlm -u : -X GET $codereview_base_url/_apis/git/repositories?api-version=2.0 2> /dev/null)
 	projects=($(echo ${json//[[:blank:]]/} | grep -o "[^\"project\"\:]{\"id\"\:\"[^\"]*\",\"name\"\:\"[^\"]*\""))
@@ -23,7 +23,7 @@ __configure () {
 		printf "${CLEAR}Configurando projeto $i de $number_of_projects.";
 		i=$((i+1))
 
-		echo "repositories_id[\"$codereview_base_url/_git/$name\"]=\"$id\"" >> $DIR/config.sh
+		echo "repositories_id[\"$codereview_base_url/_git/$name\"]=\"$id\"" >> $DIR/settings.sh
 	done
 
 	printf "${CLEAR}Projetos configurados.";
