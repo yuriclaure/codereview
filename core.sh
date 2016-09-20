66c4c0108c99047b759304b09a653c4cc604a97e
Yuri Claure <yuriclaure@gmail.com>
2016-09-20 13:55:18 -0400

    Adicionando mensagem de desativação

--- core.sh
+++ core.sh
@@ -1,5 +1,7 @@
 __codereview() {
 
+	printf "${RED}Você está usando uma versão desativada desse projeto, acesse: https://github.com/yuriclaure/tfs-pullrequest.${NC}\n";
+
 	__check_updates;
 
 	if __check_parameters "$@"; then
